e629ab8dd1c57f8356801599b47ed8936a1916d4
Chris Thapa <thapas58@gmail.com>
2019-04-24 20:48:24 +0545

    Splash screen with logo

--- app/src/main/java/com/example/bookyourvenue/MainActivity.java
+++ app/src/main/java/com/example/bookyourvenue/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.bookyourvenue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+ImageView img_splash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
