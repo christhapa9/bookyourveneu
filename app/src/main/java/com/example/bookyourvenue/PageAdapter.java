f7995141294d5ead9c6d33496d990276df3c2a66
Chris Thapa <thapas58@gmail.com>
2019-05-08 00:03:16 +0545

    dependencies added, Tablayout, splash screen, login fragment, Page adapter.

---
+++ app/src/main/java/com/example/bookyourvenue/PageAdapter.java
@@ -0,0 +1,42 @@
+package com.example.bookyourvenue;
+
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.ListFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PageAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private  final List<String> fragmentlisttitles = new ArrayList<>();
+
+
+    public PageAdapter(FragmentManager fm){
+        super (fm);
+    }
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentlisttitles.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentlisttitles.size();
+    }
+
+    public void AddFragment(Fragment fragment, String Title){
+        fragmentList.add(fragment);
+        fragmentlisttitles.add(Title);
+    }
+}
